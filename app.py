7e8a0bb47db588fa44f7e486f4084009fecda506
Oussama Berredjem <134536117+OussamaBerredjem@users.noreply.github.com>
2025-05-25 14:35:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,27 @@ from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import os
 import pickle
-from text_preprocessing import custom_preprocessor, custom_tokenizer  # Needed for unpickling
+
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
+try:
+    stop_words = set(stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
+
+def custom_preprocessor(text):
+    text = re.sub('[^a-zA-Z]', ' ', str(text))
+    return text.lower()
+
+def custom_tokenizer(text):
+    words = text.split()
+    return [ps.stem(w) for w in words if w not in stop_words]
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
